4e18c6d7375a653bbabbbb52d97752b0b33df193
nsonforwork-255 <nsonforwork@gmail.com>
2026-05-27 09:44:48 +0700

    create first job

---
+++ src/main/java/com/son/config/SampleJob.java
@@ -0,0 +1,42 @@
+package com.son.config;
+
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.StepContribution;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.info.ProjectInfoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.jspecify.annotations.Nullable;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.parameters.RunIdIncrementer;
+
+@Configuration
+public class SampleJob {
+
+	@Autowired
+	private JobRepository jobRepository;
+	
+	@Bean
+	public Job firstJob() { 
+		return new JobBuilder("firstJob", jobRepository).incrementer(new RunIdIncrementer()).start(firstStep()).build();
+	}
+	
+	public Step firstStep() {
+		return new StepBuilder("firstStep", jobRepository).tasklet(new Tasklet() {
+
+			@Override
+			public @Nullable RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+					throws Exception {
+				 System.out.println("first Step by tasklet");
+				return null;
+			}
+			
+		}).build();
+	}
+}
--- src/main/java/com/son/demo/SpingBatchApplication.java
+++ src/main/java/com/son/demo/SpingBatchApplication.java
@@ -1,9 +1,13 @@
 package com.son.demo;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@ComponentScan(basePackages =  {"com.son.config","com.son.controller", "com.son.service", "com.son.model","com.son.reader","com.son.writer"})
 public class SpingBatchApplication {
 
 	public static void main(String[] args) {
