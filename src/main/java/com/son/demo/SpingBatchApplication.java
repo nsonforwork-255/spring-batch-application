57ee925e43984cfef191a48f0567bd9a4860cc12
nsonforwork-255 <nsonforwork@gmail.com>
2026-05-27 08:40:44 +0700

    init project

---
+++ src/main/java/com/son/demo/SpingBatchApplication.java
@@ -0,0 +1,13 @@
+package com.son.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/son/demo/SpingBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.son.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
