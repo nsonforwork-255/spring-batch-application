b1bfd1e8b199904fc516d0017fb98cb1d8966423
nsonforwork-255 <nsonforwork@gmail.com>
2026-05-27 17:38:34 +0700

    added more infor csv chunk job

---
+++ src/main/java/com/son/model/CsvModel.java
@@ -0,0 +1,15 @@
+package com.son.model;
+
+import lombok.Data;
+
+@Data
+public class CsvModel {
+
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+}
